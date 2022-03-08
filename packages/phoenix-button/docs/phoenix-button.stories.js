dcc42ae54380d45ac6316651c3c410e0dcf07229
ilhamsa1 <muhammad.ilhamsa1@gmail.com>
2022-03-08 17:26:01 +0700

    feat: add initial structure for UI Library

---
+++ packages/phoenix-button/docs/phoenix-button.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// We want to always get from source
+import { Button } from '../lib/phoenix-button';
+
+export default { title: 'Button' };
+
+export const primary = () => <Button>Hello Button</Button>;
+export const secondary = () => <Button>Hello Button 2</Button>;
